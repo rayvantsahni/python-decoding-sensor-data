ebe9711c04b37d6b8ea7d7d049ba7bc5eebfaeb3
Rayvant Sahni <500061722@stu.upes.ac.in>
2021-04-17 02:54:25 +0530

    Completed module

--- sensor/energy_info.py
+++ sensor/energy_info.py
@@ -6,7 +6,9 @@ class EnergyData(HouseInfo):
     ENERGY_BITS = 0x0F0
 
     def _get_energy(self, rec):
-        energy = (int(rec, base=16) & self.ENERGY_BITS) >> 4
+        energy = int(rec, base=16)
+        energy = energy & self.ENERGY_BITS
+        energy = energy >> 4
         return energy
         
     def _convert_data(self, data):
